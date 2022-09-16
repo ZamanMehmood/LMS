17709c2daa336c1036d322e347065246d9742f53
ZamanMehmood <zaman342003@gmail.com>
2022-09-16 09:50:20 -0700

    My LMS

---
+++ jQuery/index.js
@@ -0,0 +1,46 @@
+// $(document).ready(function() {
+//     $("#basic-form").validate({
+//         rules: {
+//             email :{
+//                 required: true,
+//                 email: true
+//             }
+//         }
+       
+//     });
+//     });
+
+// $(document).ready(function() {
+//     $("#basic-form").validate({
+//         rules: {
+//                     email:"required",
+//                       password:{
+//                       required:true,
+//                       minlength:5
+//                  },
+//                  },messages:{
+                     
+//                }
+//     });
+//     });
+
+    // $(document).ready(function(){
+    //     $("#basic-form").submit(function(event){
+    //       event.preventDefault();
+    //       $("#basic-form").validate();
+    //     //   alert("Submit prevented");
+    //     });
+    //   });
+
+
+    // jQuery('#basic-form').validate({
+    //     rules: {
+    //         email:"required",
+    //         password:{
+    //             required:true,
+    //             minlength:5
+    //         },
+    //     },messages:{
+
+    //     }
+    // })
