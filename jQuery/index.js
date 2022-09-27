aa5f4ab708adab5a9fa8050b693da093aba3d209
Zaman Mehmood <zaman342003@gmail.com>
2022-09-27 15:10:30 +0500

    Work on pagination pages

--- jQuery/index.js
+++ jQuery/index.js
@@ -2,4 +2,17 @@ $(document).ready(function () {
   $("#sidebarCollapse").on("click", function () {
     $("#sidebar").toggleClass("active");
   });
+
+  $("#voucherButton1").click(function(){
+    $(".bimage1").toggle(2000);
+  });
+
+  $("#voucherButton2").click(function(){
+    $(".bimage2").toggle(2000);
+  });
+
+
+  $("#voucherButton3").click(function(){
+    $(".bimage3").toggle(2000);
+  });
 });
